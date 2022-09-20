326e0e400b32222698aa5e1626423a247338751c
Kiran Shila <me@kiranshila.com>
2022-09-19 18:28:58 -0700

    light logging

--- scripts/rfi_in_the_middle.jl
+++ scripts/rfi_in_the_middle.jl
@@ -1,4 +1,4 @@
-using PSRDADA
+using PSRDADA, Logging
 include("../src/RFIKiller.jl")
 
 const IN_KEY = 0xb0ba
@@ -10,6 +10,7 @@ const DTYPE = UInt16
 function main()
     in_client = client_connect(IN_KEY)
     out_client = client_connect(OUT_KEY)
+    @info "Connected to DADA buffers"
 
     # Only one header to relay
     with_read_iter(in_client; type=:header) do rb
@@ -18,14 +19,21 @@ function main()
         end
     end
 
+    @info "Relayed header info, starting RFI processing"
+
+    n = 0
+
     with_read_iter(in_client; type=:data) do rb
         with_write_iter(out_client; type=:data) do wb
             spectra = reshape(reinterpret(DTYPE, next(rb)), (CHANNELS, SAMPLES))
             kill_rfi!(spectra)
             next(wb) .= reinterpret(UInt8, spectra)
+            n += 1
+            @info "Processed $n chunks"
         end
     end
 
+    @info "Shutting down"
     cleanup(in_client)
     cleanup(out_client)
 end
