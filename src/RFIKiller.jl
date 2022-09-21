37ca1abf836be11032515c1702b29563e5cccddd
Kiran Shila <me@kiranshila.com>
2022-09-20 19:10:49 -0700

    Add precompile directive

--- src/RFIKiller.jl
+++ src/RFIKiller.jl
@@ -4,6 +4,9 @@ include("filters.jl")
 include("masked_stats.jl")
 include("dada_filter.jl")
 
+# Precompile to get around the LoopVectorization latency
+precompile(kill_rfi!, (Matrix{Float32}, Matrix{Bool}))
+
 export julia_main
 
 end
